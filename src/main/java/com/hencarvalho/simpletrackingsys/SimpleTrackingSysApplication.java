5e6f14f931a1eeccc0119f8e6ff260ef7928e3b3
unknown <henrysoarescarvalho@gmail.com>
2021-11-28 21:17:10 -0300

    Initial data

---
+++ src/main/java/com/hencarvalho/simpletrackingsys/SimpleTrackingSysApplication.java
@@ -0,0 +1,13 @@
+package com.hencarvalho.simpletrackingsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTrackingSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTrackingSysApplication.class, args);
+	}
+
+}
